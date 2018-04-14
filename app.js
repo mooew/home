4766e059f5887ce1349ac7e30fb7e9d51b10d3dc
mooew <wietse.gh@gmail.com>
2018-04-14 17:40:06 +0200

    test arrays

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const WebSocket = require('ws');
 var light = require('./knx.js').light
 var lightDim = require('./knx.js').lightDim
+
+var knxLightSwitch = require('./knx.js').knxLightSwitch
+var knxLightDim = require('./knx.js').knxLightDim
+
 //var temp = require('./knx.js').temp
 
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+
+
+//--------------------INCOMING--------------------------------//
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
 
@@ -14,18 +21,37 @@ wss.on('connection', function connection(ws) {
     if(obj.topic == 'switch1'){
       if(obj.payload){
         light.switchOn();
+        //knxLightSwitch[0].switchOn();
       }
       else {
         light.switchOff();
+        //knxLightSwitch[0].switchOff();
       }
     }else if(obj.topic == 'dim1'){
         lightDim.write(obj.payload);
+        //knxLightDim[0].write(obj.payload);
     }
 
 
   });
 
   ws.send('something');
+
+/*  ----------------- TO TEST -------------------
+  for( i in knxLightSwitch){
+    knxLightSwitch[i].status.on('change', function(oldvalue, newvalue) {
+      console.log("#### feedback LIGHT %j status: %j", i, newvalue);
+      var res = { topic: 'switch1', payload: newvalue }
+      ws.send(JSON.stringify(res));
+    });
+
+  for( i in knxLightDim){
+    knxLightDim[i].status.on('change', function(oldvalue, newvalue) {
+      console.log("#### feedback LIGHT %j DIM status: %j", i, newvalue);
+      var res = { topic: 'dim1', payload: newvalue }
+      ws.send(JSON.stringify(res));
+    });
+*/
 
   light.status.on('change', function(oldvalue, newvalue) {
     console.log("#### feedback LIGHT status: %j", newvalue);
--- knx.js
+++ knx.js
@@ -40,46 +40,57 @@ var connection = knx.Connection({
 var lights = [
 
   {
+  "id": 1,
   "name":"light1",
   "ga": '1/0/0',
   "status_ga": '1/0/1',
   "dim": '1/0/3',
   "status_dim": '1/0/4',
 },{
+  "id": 2,
   "name": "light2",
   "ga": '1/1/0',
   "status_ga": '1/1/1',
   "dim": '1/1/3',
   "status_dim": '1/1/4',
   },{
+  "id": 3,
   "name": "light3",
   "ga": '1/1/0',
   "status_ga": '1/1/1',
   }
 ]
 
+var knxLightSwitch = [];
+var knxLightDim = [];
+
+//---------------------connect switch lights-----------------------------//
+/*
+for(j in lights){
+  knxLightSwitch[j] = new knx.Devices.BinarySwitch(
+    {ga: lights[j].ga, status_ga: lights[j].status_ga},
+    connection
+  );
+  console.log("The light %j status is %j", j ,light.status.current_value);
+}
+//listen for status changes
+knxLights[0].status.on('change', function(oldvalue, newvalue) {
+  console.log("**** LIGHT status changed from: %j to: %j", oldvalue, newvalue);
+});
+/*/
+//---------------------connect switch lights-----------------------------//
+
+
 var light = new knx.Devices.BinarySwitch({ga: '1/0/0', status_ga: '1/0/1'}, connection);
 console.log("The current light status is %j", light.status.current_value);
 
-light.control.on('change', function(oldvalue, newvalue) {
-  //console.log("**** LIGHT control changed from: %j to: %j", oldvalue, newvalue);
-});
-light.status.on('change', function(oldvalue, newvalue) {
-  console.log("**** LIGHT status changed from: %j to: %j", oldvalue, newvalue);
-});
-//light.switchOn(); // or switchOff();
 
 //----------------------------DIM-----------------------------------------------
 
 var lightDim = new ValDimmer({ga: '1/0/3', status_ga: '1/0/4'}, connection);
 console.log("The current light status is %j", lightDim.status.current_value);
 
-lightDim.control.on('change', function(oldvalue, newvalue) {
-  //console.log("**** DIM control changed from: %j to: %j", oldvalue, newvalue);
-});
-lightDim.status.on('change', function(oldvalue, newvalue) {
-  console.log("**** DIM status changed from: %j to: %j", oldvalue, newvalue);
-});
+
 
 /*
 var temp = new Temp({ga: '0/1/1', status_ga: '0/0/4'}, connection);
@@ -98,3 +109,5 @@ module.exports.light = light
 module.exports.lightDim = lightDim
 //module.exports.temp = temp
 module.exports.connection = connection
+module.exports.knxLightSwitch = knxLightSwitch
+module.exports.knxLightDim = knxLightDim
