1ed9a91c9b0fd6557396f315ff5b6c724bb2629c
mooew <wietse.gh@gmail.com>
2018-04-22 00:12:34 +0200

    update ets 2

---
+++ ets.js
@@ -0,0 +1,57 @@
+var lights = [
+
+  {
+  "id": 1,
+  "name":"light1",
+  "ga": '1/0/0',
+  "status_ga": '1/0/1',
+  "dim": '1/0/3',
+  "status_dim": '1/0/4',
+},{
+  "id": 2,
+  "name": "light2",
+  "ga": '1/1/0',
+  "status_ga": '1/1/1',
+  "dim": '1/1/3',
+  "status_dim": '1/1/4',
+  },{
+  "id": 3,
+  "name": "light3",
+  "ga": '1/2/0',
+  "status_ga": '1/2/1',
+  },{
+  "id": 4,
+  "name": "light4",
+  "ga": '1/3/0',
+  "status_ga": '1/3/1',
+  "dim": '1/3/3',
+  "status_dim": '1/3/4',
+  },{
+  "id": 5,
+  "name": "light5",
+  "ga": '1/2/0',
+  "status_ga": '1/2/1',
+  },{
+  "id": 6,
+  "name": "light6",
+  "ga": '1/2/0',
+  "status_ga": '1/2/1',
+  },{
+  "id": 7,
+  "name": "light7",
+  "ga": '1/2/0',
+  "status_ga": '1/2/1',
+  },{
+  "id": 8,
+  "name": "light8",
+  "ga": '1/2/0',
+  "status_ga": '1/2/1',
+  },{
+  "id": 9,
+  "name": "light9",
+  "ga": '1/2/0',
+  "status_ga": '1/2/1',
+  }
+]
+
+module.exports.lights = lights;
